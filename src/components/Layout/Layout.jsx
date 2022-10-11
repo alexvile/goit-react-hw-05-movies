a92e72bb07919266f6cd0275dc5f2c099811d45f
Alexei Solotvinskiy <97736656+alexvile@users.noreply.github.com>
2022-10-11 21:19:00 +0300

    added Loader

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import { AppBar } from 'components/AppBar/AppBar';
+import { Loader } from 'components/Loader/Loader';
 export const Layout = () => {
   return (
     <>
       <AppBar />
-      <Suspense fallback={<div>LOADING</div>}>
+      <Suspense fallback={<Loader />}>
         <Outlet />
       </Suspense>
     </>
